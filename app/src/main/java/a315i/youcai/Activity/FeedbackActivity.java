96025c48ef6168aeb5dc2b4d684c21fff1092776
lanyuzx <lanyuzx@qq.com>
2017-07-13 15:54:19 +0800

    蓝雨
    小修小布

--- app/src/main/java/a315i/youcai/Activity/FeedbackActivity.java
+++ app/src/main/java/a315i/youcai/Activity/FeedbackActivity.java
@@ -2,6 +2,7 @@ package a315i.youcai.Activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import a315i.youcai.R;
 
@@ -11,5 +12,11 @@ public class FeedbackActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feedback);
+        findViewById(R.id.feedBack_backIv).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
